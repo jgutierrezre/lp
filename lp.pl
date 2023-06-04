063e611fe909dfd0c08fc377dc2b8f9bc2bd90b7
Laura Rodriguez <la.rodriguez@uniandes.edu.co>
2023-06-03 22:42:22 -0500

    done

--- lp.pl
+++ lp.pl
@@ -7,68 +7,97 @@ g(
     ]
 ).
 
-next(Node,NextNode) :-
-    g(_,G),
+% g(
+%     2,
+%     [
+%         ["a","b","c","d","e", "f", "g", "h", "i"],
+%         [e("a","b"),e("b","c"),e("c","d"),e("c","g"),e("d","e"),e("d","f"),e("g","h"),e("g","i"),e("h","i")],
+%         [v("a","a and b"),v("b","not a or c"),v("c","not c or not b"),v("d","not c"),v("e","not a"),v("f","c"),v("g","not b"),v("h","a"),v("i","b")]
+%     ]
+% ).
+
+% g(
+%     3,
+%     [
+%         ["t0","v0","u","v1","t1","s1","s0"],
+%         [e("t0","v0"),e("s0","t0"),e("s0","u"),e("u","v0"),e("u","v1"),e("s1","u"),e("s1","t1"),e("t1","u")],
+%         [v("v0","q"),v("u","p"),v("v1","q")]
+%     ]
+% ).
+
+
+
+
+
+next(Node,G, NextNode) :-
+    % g(_,G),
     [_,E,_] = G,
     member(e(Node,NextNode),E).
 
-value(Node,Value) :-
-    g(_,G),
+value(Node, G,Value) :-
+    % g(_,G),
     [_,_,V] = G,
     member(v(Node,Value),V).
 
-evaluate(Predicate,Value,Node) :-
+evaluate(Predicate,Value, G,Node) :-
     (  callable(Predicate)
-    -> call(Predicate,Node)
+    -> call(Predicate,Value, G,Node)
     ;  Predicate = Value).
 
-b(Predicate,Node) :-
+% Always
+b(Predicate, Value, G,Node) :-
     forall(
-            (next(Node,NextNode),value(NextNode,Value)),
-            evaluate(Predicate,Value,NextNode)
+            (next(Node, G,NextNode),value(NextNode, G,NewValue)),
+            evaluate(Predicate,NewValue,G, NextNode)
         ).
 
-d(Predicate,Node) :-
+% Eventually
+d(Predicate, Value, G,Node) :-
     once(
             (
-                (next(Node,NextNode),value(NextNode,Value)),
-                evaluate(Predicate,Value,NextNode)
+                (next(Node, G, NextNode),value(NextNode, G, NewValue)),
+                evaluate(Predicate,NewValue,G, NextNode)
             )
         ).
 
-% b(G,U,P) :-
-%     [N,E,V] = G,
-%     member(U,N),
-%     findall(
-%         Value,
-%         (
-%             member(e(U,Neighbor),E),
-%             member(v(Neighbor,Value),V)
-%         ),
-%         Values
-%     ),
-%     forall(
-%         member(Value,Values),
-%         Value = P
-%     ).
+and(Predicate1,Predicate2,Value, G, Node) :-
+    evaluate(Predicate1,Value, G,Node),
+    evaluate(Predicate2,Value, G,Node).
 
-% d(G,U,P) :-
-%     [N,E,V] = G,
-%     member(U,N),
-%     findall(
-%         Value,
-%         (
-%             member(e(U,Neighbor),E),
-%             member(v(Neighbor,Value),V)
-%         ),
-%         Values
-%     ),
-%     member(
-%         P,
-%         Values
-%     ).
+or(Predicate1, Predicate2, Value, G, Node) :-
+    evaluate(Predicate1,Value, G,Node);
+    evaluate(Predicate2,Value, G,Node).
 
-% valid(G,U,P) :-
-%     [N,E,V] = G,
-%     member(U,N),
+valid(G, U, P) :-
+    value(U,G,Value),
+    evaluate(P,Value,G,U).
 
+% -------------------------------------------------
+%                    EXAMPLES
+% -------------------------------------------------
+% 
+% Modal operations have only one parameter ('b' (box), 'd' (dimond), 'and' and 'or')
+
+% g(_,G),valid(G,"a",b(b("p"))).
+% output : true
+
+% g(_,G),b(b("q"),G,"a").
+% output : false
+
+% g(_,G),valid(G,"a",b(b("p"))).
+% output : true
+
+% g(_,G),valid(G,"a",b(and(b("p"),b("q")))).
+% output : false
+
+% g(_,G),valid(G,"a",b(and(b("p"),b("p")))).
+% output : true
+
+% g(_,G),valid(G,"a",b(or(b("p"),b("q"))))
+% output : false
+
+% g(_,G),valid(G,"a",b(or(b("p"),b("q")))).
+% output : true
+
+% g(_,G),valid(G,"a",b(or(b("q"),b("q")))).
+% output : false
